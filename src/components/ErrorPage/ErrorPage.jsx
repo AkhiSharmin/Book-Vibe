bbf084684bac45842bc92d3bdfe37aff372463ac
AkhiSharmin <akhisharmin100@gmail.com>
2025-03-23 00:46:06 +0600

    update navbar

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div className="max-w-7xl mx-auto mt-28">
+      <h2 className="text-9xl">404</h2>
+      <img
+        className="mx-auto"
+        src="https://i.pinimg.com/originals/70/21/10/702110d4a9c46f33fef04766cb82bd44.gif"
+        alt=""
+      />
+      <h2 className="text-5xl text-center">Page not Found</h2>
+    </div>
+  );
+};
+
+export default ErrorPage;
