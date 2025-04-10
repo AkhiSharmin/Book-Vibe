7621aefd0cde67be1797bb548e3cae901625b97a
AkhiSharmin <akhisharmin100@gmail.com>
2025-04-10 21:06:30 +0600

    added tabs

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import ErrorPage from "./components/ErrorPage/ErrorPage";
 import Home from "./components/Home/Home";
 import Dashboard from "./components/Dashboard/Dashboard";
 import BookDetail from "./components/BookDetail/BookDetail";
+import ListedBooks from "./components/ListedBooks/ListedBooks";
 
 const router = createBrowserRouter([
   {
@@ -24,6 +25,10 @@ const router = createBrowserRouter([
         path: "books/:bookId",
         element: <BookDetail></BookDetail>,
         loader: () => fetch("../public/booksData.json"),
+      },
+      {
+        path: "listedBooks",
+        element: <ListedBooks></ListedBooks>,
       },
       {
         path: "dashboard",
